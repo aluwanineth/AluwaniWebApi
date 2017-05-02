0510e3fb882fa51de7727feae2deb30b4577ff6c
Aluwani <Aluwani>
2017-05-02 21:29:58 +0200

    Add project files.

---
+++ AluwaniWebApi/Startup.cs
@@ -0,0 +1,38 @@
+﻿using AluwaniWebApi.App_Start;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AluwaniWebApi.Startup))]
+namespace AluwaniWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureAuth(app);
+
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Audience = ConfigurationManager.AppSettings["Audience"],
+                    Tenant = ConfigurationManager.AppSettings["Tenant"]
+                });
+        }
+    }
+}
